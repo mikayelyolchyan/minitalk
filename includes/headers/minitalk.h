441038f4467d2c9097699a3e53001c3b937cd154
mikayelyolchyan <mikayelyolchyan@gmail.com>
2025-05-02 16:05:57 +0400

    Create dirs and makefile

---
+++ includes/headers/minitalk.h
@@ -0,0 +1,7 @@
+# ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../libraries/libft/libft.h"
+# include "../libraries/ft_printf/ft_printf.h"
+
+#endif
---
+++ sources/client/main.c
@@ -0,0 +1,9 @@
+#include "../../includes/headers/minitalk.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+		ft_printf("Client\n");
+	return (0);
+}
