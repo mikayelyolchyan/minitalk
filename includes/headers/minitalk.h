43c223eceb9d781767a232b2d3bc4234b2af0929
nosoftplayer <nosoftplayer@kali>
2025-05-07 00:43:04 +0400

    finish

--- includes/headers/minitalk.h
+++ includes/headers/minitalk.h
@@ -5,6 +5,14 @@
 # include "../libraries/ft_printf/ft_printf.h"
 
 # include "signal.h"
+# include "unistd.h"
 # include <sys/types.h>
+
+typedef struct s_state
+{
+	pid_t	client_pid;
+	char	current_char;
+	int		bit_count;
+}	t_state;
 
 #endif
--- sources/server/main.c
+++ sources/server/main.c
@@ -1,10 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyolchy <miyolchy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 21:35:16 by miyolchy          #+#    #+#             */
+/*   Updated: 2025/05/07 00:29:59 by miyolchy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/headers/minitalk.h"
 
-int	main(void)
-{
-	pid_t pid;
+static volatile t_state	g_state = {0, 0, 0};
 
-	pid = getpid();
-	ft_printf("Server pid: %d\n", pid);
+static void	signal_handler(int signal, siginfo_t *info, void *ucontext)
+{
+	(void)ucontext;
+	(void)info;
+
+	if (signal == SIGUSR1)
+		g_state.current_char |= (1 << (7 - g_state.bit_count));
+	g_state.bit_count++;
+	if (g_state.bit_count == 8)
+	{
+		ft_printf("%c", g_state.current_char);
+		if (g_state.current_char == 0)
+			g_state.client_pid = 0;
+		g_state.current_char = 0;
+		g_state.bit_count = 0;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	struct sigaction sa;
+
+	if (argc != 1)
+	{
+		ft_printf("Error: usage ./server\n");
+		return (1);
+	}	
+	ft_printf("Server PID: %d\n", getpid());
+	sa.sa_sigaction = signal_handler;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
 	return (0);
 }
